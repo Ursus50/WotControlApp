26c6a27bed8e20f55535ce2ed367defd66803e8e
Ursus50 <mwojtowicz.poczta@gmail.com>
2024-06-25 14:03:17 +0200

    Add actions, keyboard control (W, S, A, D, SHIFT) using gestures

---
+++ actions.py
@@ -0,0 +1,51 @@
+import pydirectinput
+import time
+
+def clear_keys():
+    pydirectinput.keyUp('w')
+    pydirectinput.keyUp('a')
+    pydirectinput.keyUp('s')
+    pydirectinput.keyUp('d')
+
+def choose_action(gesture):
+    clear_keys()
+    if gesture == "Freedom":
+        pydirectinput.click()
+    elif gesture == "IndexFinger":
+        pydirectinput.keyDown('w')
+
+    elif gesture == "Thumb":
+        pydirectinput.keyDown('a')
+        # pydirectinput.keyUp('a')
+    elif gesture == "Pinky":
+        pydirectinput.keyDown('d')
+        # pydirectinput.keyUp('d')
+    elif gesture == "CallMe":
+        pydirectinput.keyDown('s')
+        # pydirectinput.keyUp('s')
+    elif gesture == "Plat":
+        pydirectinput.keyDown('shift')
+        pydirectinput.keyUp('shift')
+    elif gesture == "L":
+        pydirectinput.keyDown('w')
+        pydirectinput.keyDown('a')
+        # pydirectinput.keyUp('w')
+        # pydirectinput.keyUp('a')
+    elif gesture == "Devil":
+        pydirectinput.keyDown('w')
+        pydirectinput.keyDown('d')
+        # pydirectinput.keyUp('w')
+        # pydirectinput.keyUp('d')
+    elif gesture == "Fist":
+        pass        
+
+if __name__ == "__main__":
+    # Przykładowe użycie funkcji choose_action
+    choose_action("IndexFinger")
+
+    while True:
+        # Tutaj możesz dodać pętlę, która rejestruje gesty i wywołuje odpowiednie akcje
+        # Na przykład:
+        # gesture = funkcja_do_rejestrowania_gestów()
+        # choose_action(gesture)
+        pass
--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from mlp_model import load_model_mlp
 from cnn_model import load_model_cnn
 from utils import load_dictionary_from_file
 
+from actions import choose_action
+
+import time
 
 def get_model(type_net):
     if type_net == "mlp":
@@ -44,81 +47,94 @@ def predict_gesture(input_data):
 def get_name_gesture(number):
     pass
 
-# model = None
-model = get_model("cnn")
+if __name__ == "__main__":
+    # model = None
+    model = get_model("cnn")
 
-pathDictionary = "class_names.json"
-dictionary_gesture = load_dictionary_from_file(pathDictionary)
-# for key in dictionary_gesture:
-#     print(key)
-#     print(type(key))
-# print(dictionary_gesture)
+    pathDictionary = "class_names.json"
+    dictionary_gesture = load_dictionary_from_file(pathDictionary)
+    # for key in dictionary_gesture:
+    #     print(key)
+    #     print(type(key))
+    # print(dictionary_gesture)
 
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+    mp_hands = mp.solutions.hands
 
-# Ustawienia ekranu
-screen_width, screen_height = pyautogui.size()
+    # Ustawienia ekranu
+    screen_width, screen_height = pyautogui.size()
 
-# For webcam input:
-cap = cv2.VideoCapture(0)
-with mp_hands.Hands(
+    last_gesture = None
+    # time.sleep(5)
+    # For webcam input:
+    cap = cv2.VideoCapture(0)
+    with mp_hands.Hands(
         model_complexity=0,
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5) as hands:
-    while cap.isOpened():
-        success, image = cap.read()
-        if not success:
-            print("Ignoring empty camera frame.")
-            # If loading a video, use 'break' instead of 'continue'.
-            continue
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                # If loading a video, use 'break' instead of 'continue'.
+                continue
 
-        # To improve performance, optionally mark the image as not writeable to
-        # pass by reference.
-        image.flags.writeable = False
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = cv2.flip(image, 1)
-        results = hands.process(image)
+            # To improve performance, optionally mark the image as not writeable to
+            # pass by reference.
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            image = cv2.flip(image, 1)
+            results = hands.process(image)
 
-        # Draw the hand annotations on the image.
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            # Draw the hand annotations on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    image,
-                    hand_landmarks,
-                    mp_hands.HAND_CONNECTIONS)
-
-            if results.multi_hand_landmarks and len(results.multi_hand_landmarks) > 0:
-                list_of_points = []
+            if results.multi_hand_landmarks:
                 for hand_landmarks in results.multi_hand_landmarks:
-                    for land in hand_landmarks.landmark:
-                        list_of_points.append(land.x)
-                        list_of_points.append(land.y)
-                        list_of_points.append(land.z)
+                    mp_drawing.draw_landmarks(
+                        image,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS)
 
-                # print(len(list_of_points))
-                if len(list_of_points) == 63:
-                    gesture = predict_gesture(list_of_points)
-                    print(gesture)
-            # print(list_of_points)
+                if results.multi_hand_landmarks and len(results.multi_hand_landmarks) > 0:
+                    list_of_points = []
+                    for hand_landmarks in results.multi_hand_landmarks:
+                        for land in hand_landmarks.landmark:
+                            list_of_points.append(land.x)
+                            list_of_points.append(land.y)
+                            list_of_points.append(land.z)
 
-            # # Landmark dla indeksu (palec wskazujący)
-            # index_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
-            #
-            # # Konwersja współrzędnych do zakresu ekranu
-            # x = int(index_finger_tip.x * screen_width)
-            # y = int(index_finger_tip.y * screen_height)
-            #
-            # Sterowanie myszką
-            # pyautogui.moveTo(x, y)
+                    # print(len(list_of_points))
+                    if len(list_of_points) == 63:
+                        gesture = predict_gesture(list_of_points)
+                        print(gesture)
+                        if gesture != last_gesture:
+                            choose_action(gesture)
+                            last_gesture = gesture
 
-        # Flip the image horizontally for a selfie-view display.
-        cv2.imshow('MediaPipe Hands', image)
-        if cv2.waitKey(5) & 0xFF == 27:
-            break
-cap.release()
-cv2.destroyAllWindows()
+
+
+
+
+
+                # print(list_of_points)
+
+                # # Landmark dla indeksu (palec wskazujący)
+                # index_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+                #
+                # # Konwersja współrzędnych do zakresu ekranu
+                # x = int(index_finger_tip.x * screen_width)
+                # y = int(index_finger_tip.y * screen_height)
+                #
+                # Sterowanie myszką
+                # pyautogui.moveTo(x, y)
+
+            # Flip the image horizontally for a selfie-view display.
+            cv2.imshow('MediaPipe Hands', image)
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+    choose_action("Fist")
+    cap.release()
+    cv2.destroyAllWindows()
